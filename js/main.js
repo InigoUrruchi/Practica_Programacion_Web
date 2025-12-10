71d62cbe993acee77b4dc761cd9f5e8fc28dc7d5
Inigo <iug1002@alu.ubu.es>
2025-12-10 13:54:30 +0100

    añadir_carrito, buscador y productos ordenables

--- js/main.js
+++ js/main.js
@@ -13,31 +13,67 @@ function productos() {
 
         .then(productos_json => {
 
-            table = document.getElementById("table")
+            tbody = document.getElementById("body")
             productos_json.forEach(producto => {
 
+                id = producto._id
                 nombre = producto.name
                 precio = producto.price
                 categoria = producto.category
                 image = producto.image
                 usuario = producto.idUser
+                estado = producto.state
 
                 tr = document.createElement("tr")
                 imagen = document.createElement("td")
                 imagen.innerHTML = `<img src="${image}">`
                 descripcion = document.createElement("td")
-                
+                col_nombre = document.createElement("td")
+                col_precio = document.createElement("td")
+                col_nombre.textContent = nombre
+                col_precio.textContent = precio
+
                 descripcion.innerHTML = `
                     ${nombre}<br>
                     Precio: ${precio}<br>
-                    Categoria:${categoria}<br>`;
+                    Categoria: ${categoria}<br>
+                    Estado: ${estado}<br>
+                    <button class="btn btn-primary boton_carrito" data-id="${id}" onclick="añadir_carrito(event)">Añadir al carrito</button>`
+
+                const boton_carrito = descripcion.querySelector(".boton_carrito");
+
+                if (estado !== "En venta") {
+                    boton_carrito.disabled = true;
+                    boton_carrito.style.backgroundColor = "gray";
+                    boton_carrito.innerText = "No disponible";
+                }
 
                 tr.appendChild(imagen)
                 tr.appendChild(descripcion)
-                table.appendChild(tr)
+                tr.appendChild(col_nombre)
+                tr.appendChild(col_precio)
+                tbody.appendChild(tr)
             })
-            new DataTable("#table")
+
+            new DataTable("#table",{
+                columnDefs: [
+                    {targets: [2,3], visible: false},
+                    {targets: [2], searchable: true},
+                    {targets: [3], orderable: true},
+                    {targets: [1], orderData: [3]},
+                ]
+            })    
         })
 }
 
 document.addEventListener("DOMContentLoaded", productos);
+
+
+function añadir_carrito(event) {
+    const boton = event.target;
+    let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+    if (!carrito.includes(boton.dataset.id)) {
+        carrito.push(boton.dataset.id);
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+    }
+}
